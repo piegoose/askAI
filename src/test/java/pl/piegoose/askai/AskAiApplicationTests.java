748c85da624e5e8ea4e7182b12c5ef4df88e5580
piegoose <jedrzej.sikora@puszkin.eu>
2025-11-11 15:52:21 +0100

    Initial commit

---
+++ src/test/java/pl/piegoose/askai/AskAiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piegoose.askai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
